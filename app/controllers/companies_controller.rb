305809df603fbfb8779362bb89da8274202ab2f7
James Mastran <jam2454@columbia.edu>
2021-03-14 11:57:49 -0400

    Added function that gets distance between user (fixed address) and all the companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -8,6 +8,32 @@ class CompaniesController < ApplicationController
 
   def index
     @companies = Company.all
+    company_to_user_by_distance(5)
+  end
+
+  def company_to_user_by_distance(user_id)
+    # Add the following to the user load:
+    # geocoded_by :ip_address,
+    # :latitude => :lat, :longitude => :lon
+    # after_validation :geocode
+    @companies = Company.all
+    temp_user_address = '4200 Fifth Ave, Pittsburgh, PA 15260'
+    user_addr=Geokit::Geocoders::GoogleGeocoder.geocode temp_user_address
+    @distances = Array.new
+    @companies.each do |c|
+      if c.address.nil? == false
+        distance = Hash.new
+        distance["company_id"] = c.company_id
+        dist = user_addr.distance_to(c.address)
+        distance["distance"] = dist
+        @distances << distance
+      end
+    end
+    @distances
+  end
+
+  def get_distance(from, to)
+    from.distance_to(to)
   end
  # def initialize
     

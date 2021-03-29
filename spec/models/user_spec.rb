91dd891c16d3fc57a345f10b17e6e4d828aae44c
James Mastran <jam2454@columbia.edu>
2021-03-29 19:35:54 -0400

    Fixed test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe User, :type => :model do
     
     describe 'Get multiple products availability' do
         it "takes a list of barcodes, radius, and location and returns applicable stores" do
-            x = User.get_applicable_stores(["043765009345", "194721725820"],500,"4200 Fifth Ave, Pittsburgh, PA 15260")
-            expect(x).to eq([{"name"=>"Walmart", "address"=>"2100 88th St, North Bergen, NJ 07047", "distance"=>312.81, "total"=>869.99}])
+            x = User.get_applicable_stores(["043765009345", "194721725820"],50000,"4200 Fifth Ave, Pittsburgh, PA 15260")
+            expect(x.first()["name"]).to eq("Walmart")
         end
     end
 
